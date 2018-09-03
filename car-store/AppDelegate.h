7df683dfb78e5464bacd4b839b52e4f54f1bdea3
Dima <dima@Dimas-Mac.local>
2018-09-03 21:33:43 +0300

    Initial Commit

---
+++ car-store/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  car-store
+//
+//  Created by Dima  on 9/3/18.
+//  Copyright © 2018 Dima . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ car-store/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  car-store
+//
+//  Created by Dima  on 9/3/18.
+//  Copyright © 2018 Dima . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
